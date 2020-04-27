9d16356c1b3f79f2a0143e808082404a821e14f7
izzat <tiran.sss@mail.ru>
2020-04-27 23:27:49 +0500

    Crud system create for address done

--- Person/serializers.py
+++ Person/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from Person.models import Person, Habit, Health, Disease, Address, Region, City, District, Street
+from Person.models import Person, Habit, Health, Disease, Address, Region, City, District, Street, Building
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -90,7 +90,35 @@ class AddressSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Address
-        fields = ['id', 'name','region' ,'city', 'district', 'street', 'building']
+        fields = ['id', 'name', 'region', 'city', 'district', 'street', 'building']
+
+
+class AddressCreateSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    region = serializers.CharField()
+    city = serializers.CharField()
+    district = serializers.CharField()
+    street = serializers.CharField()
+    building = serializers.CharField()
+
+    def create(self, validated_data):
+        name = validated_data.pop('name')
+        region_id = validated_data.pop('region')
+        city_id = validated_data.pop('city')
+        district_id = validated_data.pop('district')
+        street_id = validated_data.pop('street')
+        building_id = validated_data.pop('building')
+            ############HHHHHHHHH##############
+        region = Region.objects.get(id=region_id)
+        city = City.objects.get(id=city_id)
+        district = District.objects.get(id=district_id)
+        street = Street.objects.get(id=street_id)
+        building = Building.objects.get(id=building_id)
+
+        address = Address.objects.create(region=region, city=city, district=district, street=street, building=building,
+                                         name=name)
+        print(address)
+        return address
 
 
 class PersonSerializer(serializers.ModelSerializer):

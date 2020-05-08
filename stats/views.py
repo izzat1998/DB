6909259a1461f326336fe9ff2c79b1a45e56c60c
dadakhon09 <odadaxon27@mail.ru>
2020-05-08 13:35:36 +0500

    ...

--- stats/views.py
+++ stats/views.py
@@ -21,11 +21,13 @@ class DataApiView(APIView):
         walking_list = []
         smoking_list = []
         alcohol_list = []
+        dates_list = []
 
         walking_list.append(stats.values_list('walking_meters_per_day', flat=True))
         smoking_list.append(stats.values_list('avg_num_of_cigarettes_per_day', flat=True))
         alcohol_list.append(stats.values_list('avg_amount_of_alcohol_per_day', flat=True))
-        response = [{'person_id': self.kwargs['person_id'], 'walking': w, 'smoking': s, 'alcohol': a} for
-                    w, s, a in zip(walking_list, smoking_list, alcohol_list)]
+        dates_list.append(stats.values_list('date', flat=True))
+        response = [{'person_id': self.kwargs['person_id'], 'walking': w, 'smoking': s, 'alcohol': a, 'dates': d} for
+                    w, s, a, d in zip(walking_list, smoking_list, alcohol_list, dates_list)]
 
         return Response(response)

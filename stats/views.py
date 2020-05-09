641650391478f8bf01074388cb74611e34172e7a
dadakhon09 <odadaxon27@mail.ru>
2020-05-09 17:28:42 +0500

    ...

--- stats/views.py
+++ stats/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from serializer import serialize
 
+from Person.models import Person
 from stats.models import Statistics
 from stats.serializers import StatisticsSerializer
 
@@ -17,6 +18,7 @@ class StatisticsApiView(viewsets.ModelViewSet):
 class DataApiView(APIView):
     def get(self, request, **kwargs):
         stats = Statistics.objects.filter(person_id=self.kwargs['person_id'])
+        person = Person.objects.get(id=self.kwargs['person_id'])
 
         walking_list = []
         smoking_list = []
@@ -27,7 +29,7 @@ class DataApiView(APIView):
         smoking_list.append(stats.values_list('avg_num_of_cigarettes_per_day', flat=True))
         alcohol_list.append(stats.values_list('avg_amount_of_alcohol_per_day', flat=True))
         dates_list.append(stats.values_list('date', flat=True))
-        response = [{'person_id': self.kwargs['person_id'], 'walking': w, 'smoking': s, 'alcohol': a, 'dates': d} for
+        response = [{'person_id': self.kwargs['person_id'], 'username': person.username, 'walking': w, 'smoking': s, 'alcohol': a, 'dates': d} for
                     w, s, a, d in zip(walking_list, smoking_list, alcohol_list, dates_list)]
 
         return Response(response)
